b3509c34b84e531446f8f2f8f44db428bf35dcad
Veer Chaurasiya <chaurasiyav198@gmail.com>
2026-06-17 02:32:53 +0530

    Initial Spring Boot project setup

---
+++ exception-backend/backend/exception-system/src/main/java/com/exception/system/ExceptionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.exception.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionSystemApplication.class, args);
+	}
+
+}
